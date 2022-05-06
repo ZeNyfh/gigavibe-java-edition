ddc2049745ca1bfe6cac4e6d51e9c58dccf815e5
9382 <omnomaidan@gmail.com>
2022-05-06 23:57:39 +0100

    Make MessageEvent correctly obtain properties

--- src/main/java/Bots/MessageEvent.java
+++ src/main/java/Bots/MessageEvent.java
@@ -8,12 +8,14 @@ This class is an extension of the MessageRecievedEvent that provides generally u
 As of now its a bit dry, but this may get expanded at some point when something else could be seen as useful
 -9382 */
 
-public class MessageEvent {
+public class MessageEvent extends MessageReceivedEvent {
     final String[] args;
 
     public MessageEvent(MessageReceivedEvent event) {
-        //Could be used for more at some point
-        this.args = event.getMessage().getContentRaw().split(" ");
+        //Gain the previous MRE object properties.
+        super(event.getJDA(),event.getResponseNumber(),event.getMessage()); //(Is this a good idea?)
+        //New features on-top of MRE
+        this.args = this.getMessage().getContentRaw().split(" ");
     }
 
     public String[] getArgs() { return this.args; }
