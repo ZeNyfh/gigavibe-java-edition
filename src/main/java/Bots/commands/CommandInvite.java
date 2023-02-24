da4189f1a5bdcc6d07aca85c8ca8e65096df0caf
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2023-02-24 17:16:17 +0000

    Changed URL for invite

--- src/main/java/Bots/commands/CommandInvite.java
+++ src/main/java/Bots/commands/CommandInvite.java
@@ -8,7 +8,7 @@ import static Bots.Main.createQuickEmbed;
 public class CommandInvite extends BaseCommand {
     @Override
     public void execute(MessageEvent event) {
-        event.replyEmbeds(createQuickEmbed("**Have fun!**", "http://bot.zenyfh.co.uk/"));
+        event.replyEmbeds(createQuickEmbed("**Have fun!**", "http://zenvibe.ddns.net/"));
     }
 
     @Override
