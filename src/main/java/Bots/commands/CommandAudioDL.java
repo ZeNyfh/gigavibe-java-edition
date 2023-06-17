bf36c51fa6870a9c2e418004c654e081201e1fae
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2023-06-17 05:52:29 +0100

    audiodl works on linux and windows now

--- src/main/java/Bots/commands/CommandAudioDL.java
+++ src/main/java/Bots/commands/CommandAudioDL.java
@@ -8,10 +8,12 @@ import net.dv8tion.jda.api.utils.FileUpload;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.Objects;
 
 import static Bots.Main.*;
@@ -19,163 +21,78 @@ import static Bots.Main.*;
 public class CommandAudioDL extends BaseCommand {
     @Override
     public void execute(MessageEvent event) {
+        long time = System.currentTimeMillis();
         if (event.getArgs().length < 2 || Objects.equals(event.getArgs()[1], "")) {
             event.replyEmbeds(createQuickError("No arguments given."));
             return;
         }
         final String ytdlp;
-        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-            ytdlp = "yt-dlp";
+
+        final String ffprobeString;
+        final String ffmpegString;
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            ytdlp = "modules" + File.separator + "yt-dlp.exe";
+            ffprobeString = "modules" + File.separator + "ffprobe.exe";
+            ffmpegString = "modules" + File.separator + "ffmpeg.exe";
         } else {
-            ytdlp = "modules\\yt-dlp.exe";
+            ytdlp = "yt-dlp";
+            ffprobeString = "ffprobe";
+            ffmpegString = "ffmpeg";
         }
-        File dir = new File("auddl");
+        File auddl = new File("auddl");
+        String filteredURL = event.getArgs()[1].replaceAll("\n", "");
         new Thread(() -> {
             final MessageEvent.Response[] message = new MessageEvent.Response[1];
             event.replyEmbeds(x -> message[0] = x, createQuickEmbed("Thinking...", ""));
-
-            String filename = dir.getAbsolutePath() + File.separator + System.currentTimeMillis();
-            Process p;
-            String filteredUrl = event.getArgs()[1].replaceAll("\n", "");
+            int targetSize = event.getGuild().getBoostCount() >= 7 ? 50 : 25;
+            Path inputPath = Paths.get(auddl.getAbsolutePath() + File.separator + time + ".mp3");
+            String targetFile = auddl.getAbsolutePath() + File.separator + time + "_Zen.mp3"; // filename out
             try {
-                p = Runtime.getRuntime().exec(new String[]{
-                        ytdlp, "-x", "-o", filename, "--no-playlist", filteredUrl
-                });
+                ProcessBuilder builder = new ProcessBuilder(ytdlp, "--quiet", "-x", "--audio-format", "mp3", "-o", inputPath.toString(), "--match-filter", "duration < 7200", "--no-playlist", filteredURL);
+                builder.redirectErrorStream(true);
+                Process process = builder.start();
+                process.waitFor();
+                Thread.sleep(1000);
             } catch (Exception e) {
                 e.printStackTrace();
-                message[0].editMessageEmbeds(createQuickError("Something's gone horribly wrong in the download process..."));
-                return;
+                deleteFiles("auddl" + File.separator + time);
             }
-            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line;
-            int i = 0;
-            boolean check = false;
-            try {
-                while ((line = input.readLine()) != null) {
-                    i++;
-                    if (i >= 10 && line.contains("ETA") && !check) {
-                        message[0].editMessageEmbeds(createQuickEmbed(" ", "**" + line.replaceAll("(.*?)ETA", "Approximate ETA:**")));
-                        check = true;
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                p.waitFor();
-                input.close();
-            } catch (Exception ignored) {
-            }
-            try {
-                String[] cmd;
-                if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-                    cmd = new String[]{"cmd", "/c", "dir \"" + filename + ".*\" /b /s /a:-d"};
-                } else {
-                    cmd = new String[]{"sh", "-c", "find . -name \"" + filename + ".*\" -print -quit"};
-                }
-
-                p = Runtime.getRuntime().exec(cmd);
-                p.waitFor();
-
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                    String filePath = br.readLine();
-                    if (filePath != null) {
-                        String ffmpegCmd;
-                        if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-                            ffmpegCmd = "modules\\ffmpeg.exe -loglevel error -y -i \"" + filePath + "\" -b:a 96k -f mp3 \"" + filename + ".mp3\"";
-                        } else {
-                            ffmpegCmd = "ffmpeg -loglevel error -y -i \"" + filePath + "\" -b:a 96k -f mp3 \"" + filename + ".mp3\"";
-                        }
-                        p = Runtime.getRuntime().exec(ffmpegCmd);
-                        p.waitFor();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                message[0].editMessageEmbeds(createQuickError("Something's gone horribly wrong in the conversion process..."));
-                return;
-            }
-            File finalFile = new File(filename + ".mp3");
-            float duration;
-            if (!finalFile.exists()) {
-                message[0].editMessageEmbeds(createQuickError("No file was downloaded"));
-                return;
-            }
-            //Beyond this point, we assume that activeNotice[0] just has to exist and continue on as such
-            if (finalFile.length() < 8192000 || finalFile.length() < 51200000 && event.getGuild().getBoostCount() >= 7) {
-                try {
-                    message[0].editMessageFiles(FileUpload.fromData(finalFile.getAbsoluteFile()));
-                    message[0].editMessageEmbeds(); //Remove embeds
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    message[0].editMessageEmbeds(createQuickError("The file could not be sent."));
-                    deleteFiles(filename);
-                    return;
-                }
-            } else if (finalFile.length() > 8192000) { // this is where the file resizing process happens.
-                message[0].editMessageEmbeds(createQuickEmbed(" ", "File size too large for this server, lowering bitrate..."));
-                String strDuration = "";
-                try {
-                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                        strDuration = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("modules\\ffprobe -i \"" + finalFile.getAbsolutePath() + "\" -show_entries format=duration -v quiet -of csv=\"p=0\"").getInputStream())).readLine();
-                    } else if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-                        strDuration = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("ffprobe -i " + finalFile.getAbsolutePath() + " -show_entries format=duration -v quiet -of csv=p=0").getInputStream())).readLine();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return;
-                }
-                try {
-                    duration = Float.parseFloat(strDuration); // duration of the audio file
-                } catch (Exception ignored) {
-                    message[0].editMessageEmbeds(createQuickError("Failed to get duration of track, stopping the download."));
-                    deleteFiles(filename);
-                    return;
-                }
-                long desiredBitRate = (long) (25600 / duration); // 25mb
-                if (event.getGuild().getBoostCount() > 7) {
-                    desiredBitRate = (long) (512000 / duration); // 50mb
-                }
-                if (desiredBitRate < 32) { // check for ffmpeg bitrate limit
-                    message[0].editMessageEmbeds(createQuickError("File cannot be resized to 25MB or lower."));
-                    try {
-                        deleteFiles(filename);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return;
-                } // if the desired bitrate is 32 or more
-                File finalFinalFile = new File(finalFile.getAbsolutePath().substring(0, finalFile.getAbsolutePath().length() - 4) + "K.ogg");
-                try {
-                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                        p = Runtime.getRuntime().exec("modules/ffmpeg -nostdin -loglevel error -i \"" + finalFile.getAbsolutePath() + "\" -b:a " + desiredBitRate + "k " + finalFinalFile);
-                    } else if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-                        p = Runtime.getRuntime().exec("ffmpeg -nostdin -loglevel error -i " + finalFile.getAbsolutePath() + " -b:a " + desiredBitRate + "k " + finalFinalFile);
-                    }
-                    p.waitFor();
-                    input.close();
-                } catch (Exception e) {
-                    deleteFiles(filename);
-                    e.printStackTrace();
-                }
-                message[0].editMessageFiles(FileUpload.fromData(finalFinalFile.getAbsoluteFile()));
-                message[0].editMessageEmbeds(); //Remove embeds
+            String download = "Download took: " + (System.currentTimeMillis() - time) / 1000 + " seconds.\n\n";
+            if (inputPath.toFile().length() < targetSize * 1000L * 1024L) { // does not need resizing
+                message[0].editMessageEmbeds(createQuickEmbed("✅ **Success**", download + "*if the file is not here, wait a few seconds for it to upload*"));
+                message[0].editMessageFiles(FileUpload.fromData(inputPath));
                 try {
                     Thread.sleep(10000);
-                    deleteFiles(filename);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    deleteFiles("auddl" + File.separator + time);
+                } catch (Exception ignored) {
                 }
                 return;
             }
-            message[0].editMessageFiles(FileUpload.fromData(finalFile.getAbsoluteFile()));
-            message[0].editMessageEmbeds(); //Remove embeds
+            message[0].editMessageEmbeds(createQuickEmbed("\uD83D\uDD3D **Download Complete!**", download + "Resizing, the file will be here shortly..."));
+            long newTime = System.currentTimeMillis();
             try {
+                Process process = new ProcessBuilder(ffprobeString, "-v", "error", "-show_entries", "format=duration", "-of", "csv=p=0", inputPath.toString())
+                        .redirectErrorStream(true)
+                        .start();
+                String output = new String(process.getInputStream().readAllBytes()).trim();
+
+                float originalDuration = Float.parseFloat(output);
+                long desiredBitRate = (long) (targetSize * 1024L / originalDuration);
+                if (desiredBitRate < 32) { // check for ffmpeg bitrate limit
+                    message[0].editMessageEmbeds(createQuickError("File cannot be resized to 25MB or lower."));
+                    deleteFiles("auddl" + File.separator + time);
+                    return;
+                }
+                new ProcessBuilder(ffmpegString, "-nostdin", "-loglevel", "error", "-i", inputPath.toString(), "-c:a", "aac", "-b:a", desiredBitRate + "k", targetFile).redirectErrorStream(true).start().waitFor();
+                Thread.sleep(2000);
+                String resize = "Resizing took: " + (System.currentTimeMillis() - newTime) / 1000 + " seconds.\n";
+                String downloadSpeed = "Average download speed: " + new DecimalFormat("#.##").format(new File(targetFile).length() / ((newTime - time) * 1000.0)) + "MB per second.\n\n";
+                message[0].editMessageEmbeds(createQuickEmbed("✅ **Success**", download + resize + downloadSpeed + "*if the file is not here, wait a few seconds for it to upload*"));
+                message[0].editMessageFiles(FileUpload.fromData(new File(targetFile)));
                 Thread.sleep(10000);
-                deleteFiles(filename);
+                deleteFiles("auddl" + File.separator + time);
             } catch (Exception e) {
+                deleteFiles("auddl" + File.separator + time);
                 e.printStackTrace();
             }
         }).start();
