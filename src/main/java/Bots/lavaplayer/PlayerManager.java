c13a827c673db73367d6b0cbaa67f3624a98d8ff
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2024-03-27 22:49:42 +0000

    Revert "update user feedback when error 400 occurs."
    This reverts commit 1b9e3c9fa37dc978f15e81e33d3e499f23e1fcce.

--- src/main/java/Bots/lavaplayer/PlayerManager.java
+++ src/main/java/Bots/lavaplayer/PlayerManager.java
@@ -186,17 +186,11 @@ public class PlayerManager {
                 OnCompletion.run();
             }
 
-            StringBuilder loadFailedBuilder = new StringBuilder();
+
             @Override
             public void loadFailed(FriendlyException e) {
                 clearVotes(event.getGuild().getIdLong());
-
-                if (e.getCause().getMessage().toLowerCase().contains("search response: 400")) {
-                    loadFailedBuilder.append("An error with the youtube search API has occurred.\n");
-                }
-                loadFailedBuilder.append(e.getCause().getMessage()).append("\n");
-                loadFailedBuilder.append(e.getMessage()).append("\n```");
-                event.replyEmbeds(createQuickError("The track failed to load.\n\n```\n" + loadFailedBuilder));
+                event.replyEmbeds(createQuickError("The track failed to load.\n\n```\n" + e.getMessage() + "\n```"));
                 errorlnTime("Track failed to load.\nURL: \"" + trackUrl + "\"");
                 e.printStackTrace();
                 OnCompletion.run();
