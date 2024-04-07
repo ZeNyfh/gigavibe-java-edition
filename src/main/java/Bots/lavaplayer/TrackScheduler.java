c2093e3d58799ea432ac1ff8956c03d18226d044
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2024-04-07 11:15:39 +0100

    remove import

--- src/main/java/Bots/lavaplayer/TrackScheduler.java
+++ src/main/java/Bots/lavaplayer/TrackScheduler.java
@@ -3,7 +3,6 @@ package Bots.lavaplayer;
 import Bots.MessageEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
-import com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
