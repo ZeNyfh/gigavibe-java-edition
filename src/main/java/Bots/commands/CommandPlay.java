8be2b1928f0b13d240324d6287000b799464190d
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2024-07-24 05:30:34 +0100

    fix, improve parity, and cleanup #174

--- src/main/java/Bots/commands/CommandPlay.java
+++ src/main/java/Bots/commands/CommandPlay.java
@@ -9,10 +9,12 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -55,18 +57,7 @@ public class CommandPlay extends BaseCommand {
             String fileExtension = att.getFileExtension() != null ? att.getFileExtension().toLowerCase() : "";
 
             if (fileExtension.equals("txt")) {
-                URL url = new URL(att.getUrl());
-                URLConnection connection = url.openConnection();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                try {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        PlayerManager.getInstance().loadAndPlay(event, line.split(" ", 2)[0], false);
-                    }
-                    event.replyEmbeds(createQuickEmbed("✅ **Success**", "Queued **" + att.getFileName() + "**"));
-                } catch (Exception e) {
-                    event.replyEmbeds(createQuickError("Something went wrong when loading the tracks from the file.\n```\n" + e.getMessage() + "\n```")); // tell the user what happened.
-                }
+                playFromTXT(event, true);
             } else {
                 try {
                     if (attachments.size() == 1) {
@@ -81,38 +72,15 @@ public class CommandPlay extends BaseCommand {
                     event.replyEmbeds(createQuickError("Something went wrong when loading the tracks from attachments.\n```\n" + e.getMessage() + "\n```"));
                 }
             }
-        } else { // no valid attachments to play, check for args.
+        } else { // no valid attachments to play, check for url/s in message content.
             if (args.length < 2) {
                 event.replyEmbeds(createQuickError("No arguments given."));
                 return;
             }
+            if (playFromTXT(event, false)) return;
+            // if playFromTXT fails, it tries to play the singular link.
+
             String link = String.valueOf(args[1]);
-            String[] links = link.split("http");
-            List<String> linksList = new ArrayList<>();
-            for (String str : links) {
-                linksList.add(("http" + str)
-                        .replace("youtube.com/shorts/", "youtube.com/watch?v=")
-                        .replace("youtu.be/", "www.youtube.com/watch?v=").trim()
-                );
-            }
-            int actualListSize = 0;
-            if (linksList.size() > 2) {
-                boolean antiSpam = false;
-                for (String url : linksList) {
-                    if (url.equals("http")) continue;
-                    actualListSize++;
-                    try {
-                        PlayerManager.getInstance().loadAndPlay(event, url, false);
-                    } catch (FriendlyException e) {
-                        if (!antiSpam) {
-                            antiSpam = true;
-                            event.replyEmbeds(createQuickError("Something went wrong when decoding one of the tracks.\n```\n" + e.getMessage() + "\n```"));
-                        }
-                    }
-                }
-                event.replyEmbeds(createQuickEmbed("✅ **Success**", "Queued " + actualListSize + " songs!"));
-                return;
-            }
             if (link.contains("https://") || link.contains("http://")) {
                 link = link.replace("youtube.com/shorts/", "youtube.com/watch?v=");
                 link = link.replace("youtu.be/", "www.youtube.com/watch?v=");
@@ -125,6 +93,50 @@ public class CommandPlay extends BaseCommand {
                 event.replyEmbeds(createQuickError("Something went wrong when decoding the track.\n```\n" + e.getMessage() + "\n```"));
             }
         }
+    }
+
+    private static boolean playFromTXT(MessageEvent event, boolean isAttachment) throws IOException {
+        URL url;
+        if (isAttachment) {
+            Message.Attachment att = event.getAttachments().get(0);
+            String fileExtension = att.getFileExtension() != null ? att.getFileExtension().toLowerCase() : "";
+            if (!fileExtension.equals("txt")) {
+                return false; // could be an actual audio file
+            }
+            url = new URL(att.getUrl());
+        } else {
+            String link = String.valueOf(event.getArgs()[1]);
+            if (!link.split("&")[0].split("\\?")[0].toLowerCase().endsWith(".txt")) { // could be an actual audio file
+                return false;
+            }
+            url = new URL(link);
+        }
+
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        int actualListSize = 0;
+        ArrayList<String> finalURLs = new ArrayList<>();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) continue;
+                actualListSize++;
+                line = line.trim();
+                if (!line.startsWith("http")) {
+                    line = "ytsearch: " + line;
+                }
+                finalURLs.add(line);
+            }
+            if (!isAttachment && finalURLs.size() <= 2) return false;
+            for (String finalLine : finalURLs) {
+                PlayerManager.getInstance().loadAndPlay(event, finalLine.split("\\|", 2)[0].trim(), false);
+            }
+            event.replyEmbeds(createQuickEmbed("✅ **Success**", "Queued " + actualListSize + " songs!"));
+        } catch (Exception e) {
+            event.replyEmbeds(createQuickError("Something went wrong when loading the tracks from the file.\n```\n" + e.getMessage() + "\n```")); // tell the user what happened.
+        }
+        return true;
     }
 
     @Override
