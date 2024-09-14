defba49b5de6ccf2cdabc82aea978efa369944d6
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2024-09-14 05:32:39 +0100

    localise loop

--- src/main/java/Bots/commands/CommandLoop.java
+++ src/main/java/Bots/commands/CommandLoop.java
@@ -16,10 +16,10 @@ public class CommandLoop extends BaseCommand {
     @Override
     public void execute(CommandEvent event) {
         if (LoopGuilds.contains(event.getGuild().getIdLong())) {
-            event.replyEmbeds(createQuickEmbed("❌ \uD83D\uDD01", "No longer looping the current track."));
+            event.replyEmbeds(createQuickEmbed("❌ \uD83D\uDD01", event.getLang("CommandLoop.notLooping")));
             LoopGuilds.remove(event.getGuild().getIdLong());
         } else {
-            event.replyEmbeds(createQuickEmbed("✅ \uD83D\uDD01", "Looping the current track."));
+            event.replyEmbeds(createQuickEmbed("✅ \uD83D\uDD01", event.getLang("CommandLoop.looping")));
             LoopGuilds.add(event.getGuild().getIdLong());
         }
     }
