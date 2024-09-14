ba7da603ac7c0e6af63d8e9be818d25b3706400a
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2024-09-14 05:45:20 +0100

    localised ping

--- src/main/java/Bots/commands/CommandPing.java
+++ src/main/java/Bots/commands/CommandPing.java
@@ -10,7 +10,7 @@ public class CommandPing extends BaseCommand {
     @Override
     public void execute(CommandEvent event) {
         long time = currentTimeMillis();
-        event.reply(response -> response.editMessageFormat("ping: %dms\ngateway ping: %dms", currentTimeMillis() - time, event.getJDA().getGatewayPing()), ".");
+        event.reply(response -> response.editMessageFormat(event.getLang("CommandPing.ping") + "\n" + event.getLang("CommandPing.gatewayPing"), currentTimeMillis() - time, event.getJDA().getGatewayPing()), ".");
     }
 
     @Override
